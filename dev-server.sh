34ce9c6407a92367ed8777d7425e41a109354b19
Johan <johan@blur-lasers.dev>
2026-04-07 05:39:44 +0000

    Clean up: remove internal dev files, update .gitignore

--- dev-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-while true; do
-  echo "[$(date)] Starting dev server..." >> /home/z/my-project/dev.log
-  npx next dev -p 3000 2>&1 | tee -a /home/z/my-project/dev.log
-  echo "[$(date)] Server exited, restarting in 2s..." >> /home/z/my-project/dev.log
-  sleep 2
-done
